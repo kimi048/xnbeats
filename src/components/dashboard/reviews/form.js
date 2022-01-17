b099d2a3a97a8799a81d1fb533586562d4708b05
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-01-17 20:56:00 +0900

    adding formik and yup#0

--- src/components/dashboard/reviews/form.js
+++ src/components/dashboard/reviews/form.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { Form, Button, Col } from "react-bootstrap";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
 class ReviewForm extends Component {
+  state = {};
+
   render() {
-    return <div>form</div>;
+    const state = this.state;
+    return (
+      <Formik>
+        {({ values, errors, touched, handleChange, handleSubmit }) => (
+          <h1>hoge</h1>
+        )}
+      </Formik>
+    );
   }
 }
 const mapStateToProps = (state) => {
