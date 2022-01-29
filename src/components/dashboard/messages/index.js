c22247d070ed1170c16030d3c3f7fca3d22ff122
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2022-01-30 00:52:47 +0900

    finished dashboard  messages

--- src/components/dashboard/messages/index.js
+++ src/components/dashboard/messages/index.js
@@ -7,6 +7,7 @@ class Messages extends Component {
   state = {
     posts: [],
     limit: 2,
+    count: 2,
   };
 
   componentDidMount() {
@@ -15,34 +16,75 @@ class Messages extends Component {
       .limit(this.state.limit)
       .get()
       .then((snapshot) => {
-        this.handleVars(snapshot);
+        this.handleVars(snapshot, false);
       });
   }
 
-  handleVars = (snapshot) => {
+  nextPosts = () => {
+    messagesCollection
+      .orderBy("createdAt")
+      .startAfter(this.state.last)
+      .limit(this.state.limit)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.docs.length > 0) {
+          this.handleVars(snapshot, "sum");
+        } else {
+          console.log("sorry it's the end");
+        }
+      });
+  };
+
+  prevPosts = () => {
+    if (this.state.count <= this.state.limit) {
+      console.log("sorry dude, no more posts");
+    } else {
+      messagesCollection
+        .orderBy("createdAt")
+        .endBefore(this.state.last)
+        .limit(this.state.limit)
+        .get()
+        .then((snapshot) => {
+          this.handleVars(snapshot, "rest");
+        });
+    }
+  };
+
+  handleVars = (snapshot, action) => {
+    const { count } = this.state;
     const { docs } = snapshot;
     const posts = docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
+
+    const lastVisible = docs[docs.length - 1];
+    const firstVisible = docs[0];
+    const restSum =
+      action === "sum" ? count + docs.length : count - docs.length;
+
     this.setState((prev) => ({
       posts: posts,
+      last: lastVisible,
+      first: firstVisible,
+      count: !action ? prev.count : restSum,
     }));
   };
 
   render() {
+    console.log(this.state);
     return (
       <DashLayout auth={this.props.auth} title="Messages">
-        {
-          this.state.posts.map(item=>(
-            <CardMessage
-            data={item} 
-            key={item.id}/>
-          ))
-        }
+        {this.state.posts.map((item) => (
+          <CardMessage data={item} key={item.id} />
+        ))}
         <div className="mt-3">
-          <div className="btn btn-primary mr-2">PREV</div>
-          <div className="btn btn-primary mr-2">NEXT</div>
+          <div className="btn btn-primary mr-2" onClick={this.prevPosts}>
+            PREV
+          </div>
+          <div className="btn btn-primary mr-2" onClick={this.nextPosts}>
+            NEXT
+          </div>
         </div>
       </DashLayout>
     );
